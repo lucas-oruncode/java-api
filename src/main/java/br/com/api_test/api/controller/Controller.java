79359c1e3eee4e67e278822efdd14828d4c93a78
lucas <oruncode@gmail.com>
2025-01-21 18:08:12 -0300

    feat: Cria objeto com @autowired para instanciar as açoes CRUD

--- src/main/java/br/com/api_test/api/controller/Controller.java
+++ src/main/java/br/com/api_test/api/controller/Controller.java
@@ -1,5 +1,6 @@
 package br.com.api_test.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api_test.api.models.Pessoa;
+import br.com.api_test.api.repository.Repo;
 
 @RestController
 public class Controller {
+    
+    @Autowired
+    private Repo action;
     
     @GetMapping("")
     public String mensagem() {
